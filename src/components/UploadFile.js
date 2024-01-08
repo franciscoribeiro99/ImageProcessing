f1a06edbcf9b5c5659fc43a6644ef4a2f16e4606
dij0s <redrouge03@gmail.com>
2024-01-08 13:55:06 +0100

    First commit, landing page is ready..

---
+++ src/components/UploadFile.js
@@ -0,0 +1,75 @@
+import './UploadFile.css'
+
+import { useState, useEffect } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+export default function UploadFile() {
+    const [ fileDataURL, setFileDataURL ] = useState(null)
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {
+            'image/jpeg': [],
+            'image/png': []
+        },
+        onDrop: (acceptedFile) => {
+            const fileReader = new FileReader()
+            
+            fileReader.onabort = () => console.log('File reading was aborted.')
+            fileReader.onerror = () => console.log('File reading has failed.')
+            fileReader.onload = (e) => {
+                setFileDataURL(e?.target?.result)
+            }
+            
+            fileReader.readAsDataURL(acceptedFile[0])
+        }
+    })
+
+    useEffect(() => {
+        if (fileDataURL !== null) {
+            // fetch("http://localhost:8000/layers", {
+            //     method: 'post',
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     mode: 'cors',
+            //     body: JSON.stringify({
+            //         "image": fileDataURL
+            //     })
+            // })
+        }
+    }, [fileDataURL])
+
+    return (
+        <div id='upload-file-wrapper' {...getRootProps()}>
+            <input {...getInputProps()} />
+
+            { 
+                fileDataURL === null ?
+                <>
+                    <img id='upload-file-icon' alt='upload-file-icon' src='https://cdn3.iconfinder.com/data/icons/file-and-folder-fill-icons-set/144/File_Upload-512.png'></img>
+                    <div id='upload-file-text-wrapper'>
+                        <h3 id='upload-file-text-title'>
+                            Drop your image here or browse.
+                        </h3>
+                        <h3 id='upload-file-text-meta'>
+                            Supports PNG, JPG and JPEG.
+                        </h3>
+                    </div>
+                </>
+                :
+                <div id="upload-file-uploaded-wrapper">
+                    <img id="upload-file-uploaded" src={fileDataURL} alt=''></img>
+                    <div id="upload-file-uploaded-layers">
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                        <img className="upload-file-uploaded-layer" src={fileDataURL} alt=''></img>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
---
+++ src/components/embed/HeroDescription.js
@@ -0,0 +1,10 @@
+import './HeroDescription.css'
+
+export default function HeroDescription() {
+    return (
+        <div id='hero-description-wrapper'>
+            <h1 id='hero-description-title'>Embed and hide any text into your image.</h1>
+            <h2 id='hero-description-meta'>Please first upload your file.</h2>
+        </div>
+    )
+}
