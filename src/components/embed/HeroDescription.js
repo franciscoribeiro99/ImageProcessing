5435dfa0fc1df577a5ea6badb2243eb668517a6c
dij0s <redrouge03@gmail.com>
2024-01-08 16:02:58 +0100

    Upload page is ready for use..

--- src/components/embed/HeroDescription.js
+++ src/components/embed/HeroDescription.js
@@ -5,6 +5,7 @@ export default function HeroDescription() {
         <div id='hero-description-wrapper'>
             <h1 id='hero-description-title'>Embed and hide any text into your image.</h1>
             <h2 id='hero-description-meta'>Please first upload your file.</h2>
+            <progress id='hero-progress-bar' max={100}></progress>
         </div>
     )
 }
