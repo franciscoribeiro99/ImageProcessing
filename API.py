8284bb84c3fcca73b7f6cbaccd49698a011b00fc
MartaRende <121174493+MartaRende@users.noreply.github.com>
2024-01-17 10:52:06 +0100

    updating of API

--- API.py
+++ API.py
@@ -1,10 +1,16 @@
+import os
+
+from flask import Flask, request, send_file, send_from_directory
 from flask import Flask, request, send_file
 import ImageProcessing
 
 app = Flask(__name__)
 
+@app.route('/status', methods=['GET'])
+def status():
+    return '', 200
 
-@app.route('/embed_secret', methods=['POST'])
+@app.route('/embed_secret', methods=['GET', 'POST'])
 def embed_secret():
     data = request.json
     cover_image_path = data['cover_image_path']
@@ -13,22 +19,44 @@ def embed_secret():
 
     #function to embed the secret
     ImageProcessing.embed_secret(cover_image_path, secret_text, output_path)
+    layer_paths = data.get('layers', [])
+    for layer in layer_paths:
+        send_from_directory(os.path.dirname(layer), os.path.basename(layer), as_attachment=True)
 
+    layer_paths = 'layers'
+    for layer in layer_paths:
+        os.remove(f'{layer_paths}/{layer}')
+    os.remove('output/extractedText.png')
+    os.remove('imageWithHiddenText/extractedText.png')
+    os.remove('imagesInput/image.png')
     # Return the path of the modified image
-    return send_file(output_path, as_attachment=True)
+    return 'done'
 
 
-@app.route('/extract_secret', methods=['POST'])
+@app.route('/extract_secret', methods=['GET', 'POST'])
 def extract_secret():
     data = request.json
     stego_image_path = data['stego_image_path']
     output_path = data['output_path']
 
-    #function to exract the secret
     ImageProcessing.extract_secret(stego_image_path, output_path)
 
+    send_file(output_path, as_attachment=True)
+
+
+    layers = data.get('layers', [])
+    for layer in layers:
+        send_from_directory(os.path.dirname(layer), os.path.basename(layer), as_attachment=True)
+
+    layer_paths = 'layers'
+    for layer in layer_paths:
+        os.remove(f'{layer_paths}/{layer}')
+    os.remove('output/extractedText.png')
+    os.remove('imageWithHiddenText/extractedText.png')
+    os.remove('imagesInput/image.png')
     # Return the path of the extracted image
-    return send_file(output_path, as_attachment=True)
+    return 'done'
+
 
 
 if __name__ == '__main__':
